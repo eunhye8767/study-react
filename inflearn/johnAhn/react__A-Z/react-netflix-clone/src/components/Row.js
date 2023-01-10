e377d27fd767d3329a0dbde2c8e3c6e013722e17
leh87 <ehlee@brainednet.com>
2023-01-10 21:03:40 +0900

    [reactA_Z] 모달 팝업

--- inflearn/johnAhn/react__A-Z/react-netflix-clone/src/components/Row.js
+++ inflearn/johnAhn/react__A-Z/react-netflix-clone/src/components/Row.js
@@ -1,13 +1,20 @@
 import axios from "../api/axios"
 import React, { useEffect, useState } from 'react'
 
+import MovieModal from "./MovieModal";
+
 import "./Row.css"
 
 export default function Row({ title, id, fetchUrl, isLargeRow }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/"
 
   // 영화 정보 가져오기 (1)
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+
+  // 이미지 클릭 => 모달 팝업 (2)
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
+
 
   // 영화 정보 가져오기 (2)
   useEffect(() => {
@@ -19,6 +26,12 @@ export default function Row({ title, id, fetchUrl, isLargeRow }) {
     const request = await axios.get(fetchUrl);
     setMovies(request.data.results);
     return request;
+  }
+
+  // 이미지 클릭 => 모달 팝업 (1)
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
   }
 
   return (
@@ -43,6 +56,7 @@ export default function Row({ title, id, fetchUrl, isLargeRow }) {
               src={`${BASE_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
               loading="lazy"
               alt={movie.name}
+              onClick={ ()=> handleClick(movie) }
             />
           ))}
         </div>
@@ -55,6 +69,16 @@ export default function Row({ title, id, fetchUrl, isLargeRow }) {
             >{">"}</span>
         </div>
       </div>
+
+      {/* // 이미지 클릭 => 모달 팝업 (3) */}
+      {
+        modalOpen && (
+          <MovieModal
+            {...movieSelected}
+            BASE_URL={BASE_URL}
+            setModalOpen={setModalOpen} />
+        )
+      }
     </section>
   )
 }
