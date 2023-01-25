3e1a524f61a1bcb5081351aad4fb98848e34e319
leh87 <ehlee@brainednet.com>
2023-01-25 21:01:19 +0900

    [reactA_Z] swiper 모듈을 이용한 터치 슬라이드 구현하기

--- inflearn/johnAhn/react__A-Z/react-netflix-clone/src/components/Row.js
+++ inflearn/johnAhn/react__A-Z/react-netflix-clone/src/components/Row.js
@@ -5,6 +5,17 @@ import MovieModal from "./MovieModal";
 
 import "./Row.css"
 
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 export default function Row({ title, id, fetchUrl, isLargeRow }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/"
 
@@ -38,37 +49,47 @@ export default function Row({ title, id, fetchUrl, isLargeRow }) {
     <section className="row">
       {/* title */}
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span 
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft -= window.innerWidth - 80
-            }}
-            >{"<"}</span>
-        </div>
+
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true}
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+      >
         <div id={id} className="row__posters">
           {/* SEVERAL ROW__POSTER */}
           {movies.map((movie) => (
-            <img 
-              key={movie.id}
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`${BASE_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-              loading="lazy"
-              alt={movie.name}
-              onClick={ ()=> handleClick(movie) }
-            />
+            <SwiperSlide key={movie.id}>
+              <img 
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`${BASE_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                loading="lazy"
+                alt={movie.name}
+                onClick={ ()=> handleClick(movie) }
+              />
+            </SwiperSlide>
           ))}
         </div>
-        <div className="slider__arrow-right">
-          <span 
-            className="arrow" 
-            onClick={() => {
-              document.getElementById(id).scrollLeft += window.innerWidth - 80
-            }}
-            >{">"}</span>
-        </div>
-      </div>
+      </Swiper>
 
       {/* // 이미지 클릭 => 모달 팝업 (3) */}
       {
