8281704e01d2418543e25031c1458e521afecbea
leh87 <ehlee@brainednet.com>
2023-02-01 21:06:53 +0900

    [reactA_Z] 앱 만들기 - .test.js 주석 설명

--- inflearn/johnAhn/react__A-Z/react-button-app/src/App.test.js
+++ inflearn/johnAhn/react__A-Z/react-button-app/src/App.test.js
@@ -3,6 +3,10 @@ import App from './App';
 
 test('the counter starts at 0', () => {
   render(<App />);
+  // screen object를 이용해서 원하는 엘리먼트에 접근 (접근할 때 ID로 접근)
   const countElement = screen.getByTestId("counter");
+  // id가 counter인 엘리먼트의 텍스트가 0인지 테스트
   expect(countElement).toHaveTextContent(0);
 });
+
+
