f48251d9a3f1d68d8956fbb925115d29e90da96a
leh87 <ehlee@brainednet.com>
2023-02-14 13:41:03 +0900

    [reactA_Z] 미들웨어 없이 리덕스 카운터 앱 만들기

---
+++ inflearn/johnAhn/react__A-Z/react-redux-app/src/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+/**
+ * redux에선 리덕스툴팁 방식을 권장하기 때문에
+ * 현재 createStore는 밑줄이 그어져있다.
+ */
+import { createStore } from "redux";
+import counter from "./reducers";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+const store = createStore(counter);
+const render = () => root.render(
+  <React.StrictMode>
+    <App
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+      onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+    />
+  </React.StrictMode>
+);
+
+render();
+store.subscribe(render);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
