b4cfd0122afc28cd180da6710f02434ae9764182
leh87 <ehlee@brainednet.com>
2023-02-15 15:17:19 +0900

    [코지코더] useState

--- inflearn/kossiecoder/project_react/README.md
+++ inflearn/kossiecoder/project_react/README.md
@@ -1,2 +1,20 @@
 # 프로젝트로 배우는 React.js
 
+#### 1. useState(prevState)
+```javascript
+import { useState } from 'react';
+
+const [number, setNumber] = useState(1);
+
+const handleChange = () => {
+  setNumber(prevState => prevState * 2);
+  console.log(number)
+}
+```
+
+- `prevState` <br >
+=> 작명 가능하나 통상적으로 prevState 칭함.<br />
+=> 이전 state 값을 가져와서 변경할 수 있음.
+- `console.log(number)` <br />
+=> 바뀐 useState 값이 나오는 것이 아닌 이전 `number` 값이 나온다.<br />
+=> 리-렌더링 후 바뀌기 때문에 바뀌기 전 `number` 값이 나오는 것이다.
--- inflearn/kossiecoder/project_react/react-basic/src/index.js
+++ inflearn/kossiecoder/project_react/react-basic/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+/**
+ *  # reportWebVitals 
+ *     ㄴ 리액트의 성능을 측정하기 위한 것.
+ */
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +19,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
