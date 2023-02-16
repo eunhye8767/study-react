ff06079b8ca7140663214059b614948e837281cc
leh87 <ehlee@brainednet.com>
2023-02-16 18:08:51 +0900

    [코지코더] navbar 컴포넌트화

--- inflearn/kossiecoder/project_react/react-basic/src/App.js
+++ inflearn/kossiecoder/project_react/react-basic/src/App.js
@@ -2,24 +2,15 @@ import {
   BrowserRouter as Router, // BrowserRouter => Router로 사용
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
 import BlogForm from "./components/BlogForm";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Router>
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="container">
-          <Link className="navbar-brand" to="/">Home</Link>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/blogs">Blogs</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <NavBar />
 
       <div className="container">
         <Switch>
