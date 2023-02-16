b07e4adc2c2b283578ef4a6446e656c77f016c3d
leh87 <ehlee@brainednet.com>
2023-02-16 18:04:23 +0900

    [코지코더] 블로그폼 컴포넌트화

--- inflearn/kossiecoder/project_react/react-basic/src/App.js
+++ inflearn/kossiecoder/project_react/react-basic/src/App.js
@@ -1,24 +1,13 @@
-import { useState } from "react";
-import axios from "axios";
 import {
   BrowserRouter as Router, // BrowserRouter => Router로 사용
   Switch,
   Route,
   Link
-} from "react-router-dom"
+} from "react-router-dom";
+
+import BlogForm from "./components/BlogForm";
 
 function App() {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-
-  const onSubmit = () => {
-    axios.post("http://localhost:3001/posts", {
-      // 보낼 데이터 영역
-      title,
-      body
-    })
-  }
-
   return (
     <Router>
       <nav className="navbar navbar-dark bg-dark">
@@ -32,34 +21,14 @@ function App() {
         </div>
       </nav>
 
-      <Switch>
-        <Route path="/" exact>Home page</Route>
-        <Route path="/blogs">
-          <div className="container">
-            <h1 className="mt-3 mb-3">Create a blog post</h1>
-            <div className="mb-3">
-              <label className="form-label">Title</label>
-              <input
-                className="form-control"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </div>
-            
-            <div className="mb-3">
-              <label className="form-label">Body</label>
-              <textarea
-                className="form-control"
-                value={body}
-                onChange={(e) => setBody(e.target.value)}
-                rows="10"
-              />
-            </div>
-
-            <button className="btn btn-primary" onClick={onSubmit}>Post</button>
-          </div>
-        </Route>
-      </Switch>
+      <div className="container">
+        <Switch>
+          <Route path="/" exact>Home page</Route>
+          <Route path="/blogs">
+            <BlogForm />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
---
+++ inflearn/kossiecoder/project_react/react-basic/src/components/BlogForm.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import axios from "axios";
+
+const BlogForm = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const onSubmit = () => {
+    axios.post("http://localhost:3001/posts", {
+      // 보낼 데이터 영역
+      title,
+      body
+    })
+  }
+
+  return (
+    <div>
+      <h1 className="mt-3 mb-3">Create a blog post</h1>
+      <div className="mb-3">
+        <label className="form-label">Title</label>
+        <input
+          className="form-control"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      
+      <div className="mb-3">
+        <label className="form-label">Body</label>
+        <textarea
+          className="form-control"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          rows="10"
+        />
+      </div>
+
+      <button className="btn btn-primary" onClick={onSubmit}>Post</button>
+    </div>
+  )
+}
+
+export default BlogForm
