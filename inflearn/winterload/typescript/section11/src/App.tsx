7388cc06320741cf85fb6ab2bcb1d6f44f264d4a
leh87 <ehlee@brainednet.com>
2023-07-12 21:12:50 +0900

    [winterload] Todo - useState -> reducer로 변환

--- inflearn/winterload/typescript/section11/src/App.tsx
+++ inflearn/winterload/typescript/section11/src/App.tsx
@@ -1,28 +1,57 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import TodoItem from "./components/TodoItem";
 
 import { Todo } from "./types";
+import { type } from "os";
+
+type Action =
+  | {
+      type: "CREATE";
+      data: {
+        id: number;
+        content: string;
+      };
+    }
+  | {
+      type: "DELETE";
+      id: number;
+    };
+
+function reducer(state: Todo[], action: Action) {
+  switch(action.type) {
+    case "CREATE": {
+      return [...state, action.data]
+    }
+
+    case "DELETE": {
+      return state.filter((it) => it.id !== action.id)
+    }
+  }
+}
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, []);
 
   const idRef = useRef(0);
 
   const onClickAdd = (text: string) => {
-    setTodos([
-      ...todos,
-      {
+    dispatch({
+      type: "CREATE",
+      data: {
         id: idRef.current++,
         content: text,
       },
-    ]);
+    });
   };
 
   const onClickDelete = (id: number) => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  }
+    dispatch({
+      type: "DELETE",
+      id: id,
+    });
+  };
 
   useEffect(() => {
     console.log(todos);
@@ -36,7 +65,7 @@ function App() {
       </Editor>
 
       <hr />
-      
+
       <div>
         {todos.map((todo) => (
           <TodoItem key={todo.id} {...todo} onClickDelete={onClickDelete} />
