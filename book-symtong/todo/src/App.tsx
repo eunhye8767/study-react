107363c2911ee57b7dd1706eb723f804f523d4a7
leh87 <ehlee@brainednet.com>
2023-08-25 18:58:06 +0900

    ShowInputButton

--- book-symtong/todo/src/App.tsx
+++ book-symtong/todo/src/App.tsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled';
 
 import { DataView } from 'components/DataView';
 import { TodoInput } from 'components/TodoInput';
+import { ShowInputButton } from 'components/ShowInputButton';
 
 const Container = styled.div`
   display: flex;
@@ -20,9 +21,11 @@ function App() {
     '타입스크립트와 리액트',
     '공부를 해야겠지요',
   ]);
+  const [showToDoInput, setShowToDoInput] = useState(false);
 
   const onAdd = (toDo: string) => {
     setToDoList([...toDoList, toDo]);
+    setShowToDoInput(false);
   };
 
   const onDelete = (todo: string) => {
@@ -32,7 +35,8 @@ function App() {
   return (
     <Container>
       <DataView toDoList={toDoList} onDelete={onDelete} />
-      <TodoInput onAdd={onAdd} />
+      {showToDoInput && <TodoInput onAdd={onAdd} />}
+      <ShowInputButton show={showToDoInput} onClick={() => setShowToDoInput(!showToDoInput)} />
     </Container>
   );
 }
